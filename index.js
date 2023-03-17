0513547daddaca19220ddc5261dc84306c33782f
luis <lugo50635@gmail.comm>
2023-03-17 13:51:09 -0300

    first day of work

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import informatica from './studyPlans/informatica.json' assert { type: 'json' }
+
+const careers = [informatica]
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('hello wordl')
+})
+
+app.get('/:career', (req, res) => {
+  const { career } = req.params
+  const lookedCareer = careers.find(el => el.careerName === career)
+  if (lookedCareer) return res.json(lookedCareer)
+  res.json({ careerExists: false })
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
